dc2dfc9fcd8fdf6621aa93c1c82d664bf51c6b8f
Guilherme Toti <guilhermetoti@gmail.com>
2023-08-21 23:36:12 -0300

    changes

--- new/fix_api_client.py
+++ new/fix_api_client.py
@@ -25,133 +25,155 @@ trading_session_credentials = {
     "reset_on_logon": "N",
 }
 
+
 class FixApiClient:
 
-  def __init__(self):
-    self.username = default_credentials["username"]
-    self.server = default_credentials["server"]
-    self.target_comp_id = default_credentials["target_comp_id"]
-    self.pricing_session = self.get_pricing_session()
-    self.trading_session = self.get_trading_session()
-    self.sequence_number = 1
+    def __init__(self):
+        self.username = default_credentials["username"]
+        self.server = default_credentials["server"]
+        self.target_comp_id = default_credentials["target_comp_id"]
+        self.pricing_session = self.get_pricing_session()
+        self.trading_session = self.get_trading_session()
+        self.sequence_number = 1
 
-  @staticmethod
-  def get_fix_message():
-    return simplefix.FixMessage()
+    @staticmethod
+    def get_fix_message():
+        return simplefix.FixMessage()
 
-  def get_pricing_session(self):
-    return self._get_session(pricing_session_credentials)
-  
-  def get_trading_session(self):
-    return self._get_session(trading_session_credentials)
-  
-  def _get_session(self, credentials):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((self.server, credentials["port"]))
-    return sock
+    @staticmethod
+    def listen_to_response(session):
+        parser = simplefix.parser.FixParser()
 
-  def send_message(self, session, **kwargs):
-    fix_message = self.build_message(**kwargs)
-    
-    try:
-      session.sendall(fix_message)
-    except BrokenPipeError:
-      print("BrokenPipeError: disconnected")
-      return None
+        data = session.recv(8192)
 
-    print(f"Sending message: {fix_message}")
+        parser.append_buffer(data)
 
-    self.sequence_number += 1
+        return parser.get_message()
 
-    return self.listen_to_response(session)
+    def get_pricing_session(self):
+        return self._get_session(pricing_session_credentials)
 
-  def send_pricing_session(self, **kwargs):
-    headers = kwargs.get('headers') or []
-    parameters = kwargs.get('parameters') or []
-    headers += [f"49={pricing_session_credentials['sender_comp_id']}"]
+    def get_trading_session(self):
+        return self._get_session(trading_session_credentials)
 
-    return self.send_message(session=self.pricing_session, headers=headers, parameters=parameters)
+    def _get_session(self, credentials):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.server, credentials["port"]))
+        return sock
 
-  # def send_trading_session(self, parameters):
-  #   parameters += [f"49={trading_session_credentials['sender_comp_id']}"]
+    def send_message(self, session, **kwargs):
+        fix_message = self.build_message(**kwargs)
 
-  #   return self.send_message(self.trading_session, parameters)
+        try:
+            session.sendall(fix_message)
+        except BrokenPipeError:
+            print("BrokenPipeError: disconnected")
+            return None
 
-  def listen_to_response(self, session):
-    parser = simplefix.parser.FixParser()
+        print(f"Sending message: {fix_message}")
 
-    data = session.recv(8192)
+        self.sequence_number += 1
 
-    parser.append_buffer(data)
+        return self.listen_to_response(session)
 
-    return parser.get_message()
+    def send_pricing_session(self, **kwargs):
+        headers = kwargs.get('headers') or []
+        parameters = kwargs.get('parameters') or []
+        headers += [f"49={pricing_session_credentials['sender_comp_id']}"]
 
-  def build_message(self, **kwargs):
-    headers = kwargs.get('headers')
-    parameters = kwargs.get('parameters')
+        return self.send_message(session=self.pricing_session, headers=headers, parameters=parameters)
 
-    fix_message = self.get_fix_message()
-    fix_message.append_pair(8, "FIX.4.4")
-    fix_message.append_pair(56, self.target_comp_id)
-    fix_message.append_pair(34, self.sequence_number)
-    fix_message.append_time(52)
-    fix_message.append_strings(headers, header=True)
+    def send_trading_session(self, **kwargs):
+        headers = kwargs.get('headers') or []
+        parameters = kwargs.get('parameters') or []
+        headers += [f"49={pricing_session_credentials['sender_comp_id']}"]
 
-    fix_message.append_strings(parameters)
+        return self.send_message(session=self.trading_session, headers=headers, parameters=parameters)
 
-    return fix_message.encode()
+    def build_message(self, **kwargs):
+        headers = kwargs.get('headers')
+        parameters = kwargs.get('parameters')
 
-  def heartbeat(self):
-    headers = ["35=0"]
+        fix_message = self.get_fix_message()
+        fix_message.append_pair(8, "FIX.4.4")
+        fix_message.append_pair(56, self.target_comp_id)
+        fix_message.append_pair(34, self.sequence_number)
+        fix_message.append_time(52)
+        fix_message.append_strings(headers, header=True)
 
-    return self.send_pricing_session(headers=headers)
+        fix_message.append_strings(parameters)
 
-  def test_request(self):
-    headers = ["35=1"]
-    parameters = [f"112={uuid4()}"]
-    return self.send_pricing_session(headers=headers, parameters=parameters)
+        return fix_message.encode()
 
-  def logon(self):
-    headers = [
-      "35=A",
-    ]
+    def heartbeat(self):
+        headers = ["35=0"]
 
-    parameters = [
-      "98=0",
-      "108=30",
-      "141=Y",
-      f"553={self.username}",
-      f"554={pricing_session_credentials['password']}",
-    ]
+        return self.send_pricing_session(headers=headers)
 
-    return self.send_pricing_session(headers=headers, parameters=parameters)
+    def test_request(self):
+        headers = ["35=1"]
+        parameters = [f"112={uuid4()}"]
+        return self.send_pricing_session(headers=headers, parameters=parameters)
 
-  def logout(self):
-    headers = ["35=5"]
+    def logon(self):
+        headers = [
+            "35=A",
+        ]
 
-    return self.send_pricing_session(headers=headers)
+        parameters = [
+            "98=0",
+            "108=30",
+            "141=Y",
+            f"553={self.username}",
+            f"554={pricing_session_credentials['password']}",
+        ]
 
-  def resend_request(self):
-    headers = ["35=2"]
-    parameters = ["7=0", "16=0"]
+        return self.send_pricing_session(headers=headers, parameters=parameters)
 
-    return self.send_pricing_session(headers=headers, parameters=parameters)
+    def logout(self):
+        headers = ["35=5"]
 
-  def reject(self):
-    headers = ["35=3"]
-    parameters = ["45=1"]
+        return self.send_pricing_session(headers=headers)
 
-    return self.send_pricing_session(headers=headers, parameters=parameters)
+    def resend_request(self):
+        headers = ["35=2"]
+        parameters = ["7=0", "16=0"]
 
-  def business_reject(self):
-    headers = ["35=j"]
-    """Preciso entender melhor oq é isso e quando é usado"""
-    parameters = ["45=1"]
+        return self.send_pricing_session(headers=headers, parameters=parameters)
 
-    return self.send_pricing_session(headers=headers, parameters=parameters)
+    def reject(self):
+        headers = ["35=3"]
+        parameters = ["45=3"]
 
-  def sequence_reset(self):
-    headers = ["35=4"]
-    parameters = ["36=1"]
+        return self.send_pricing_session(headers=headers, parameters=parameters)
 
-    return self.send_pricing_session(headers=headers, parameters=parameters)
+    def business_reject(self):
+        headers = ["35=j"]
+        """Preciso entender melhor oq é isso e quando é usado"""
+        parameters = ["45=1"]
+
+        return self.send_pricing_session(headers=headers, parameters=parameters)
+
+    def sequence_reset(self):
+        headers = ["35=4"]
+        parameters = ["36=1"]
+
+        return self.send_pricing_session(headers=headers, parameters=parameters)
+
+    # Market data requests
+    def market_data_request(self, symbol: str, request_id: str = None):
+        if not request_id:
+            request_id = uuid4()
+
+        headers = ["35=V"]
+        parameters = [
+            f"262={request_id}",
+            "263=1",
+            "264=0",
+            "267=1",
+            "269=1",
+            "146=1",
+            f"55={symbol}",
+        ]
+
+        return self.send_pricing_session(headers=headers, parameters=parameters)
--- new/main.py
+++ new/main.py
@@ -1,32 +1,57 @@
 import time
+from uuid import uuid4
 
 from fix_api_client import FixApiClient
 
 if __name__ == '__main__':
-  fix_api_client = FixApiClient()
+    fix_api_client = FixApiClient()
 
-  print(f"Logon {fix_api_client.logon()}")
+    print(f"Logon {fix_api_client.logon()}")
+    #
+    time.sleep(.2)
+    #
+    # print(f"heartbeat: {fix_api_client.heartbeat()}")
+    #
+    # time.sleep(.2)
+    #
+    # print(f"test_request: {fix_api_client.test_request()}")
+    #
+    # time.sleep(.2)
+    #
+    # print(f"resend_request: {fix_api_client.resend_request()}")
+    #
+    # time.sleep(.2)
+    #
+    # print(f"reject: {fix_api_client.reject()}")
+    #
+    # time.sleep(.2)
+    #
+    # print(f"sequence_reset: {fix_api_client.sequence_reset()}")
+    #
+    # time.sleep(.2)
+    #
+    # print(f"logout: {fix_api_client.logout()}")
 
-  time.sleep(.5)
+    # Market Data Requests
+    start_time = time.perf_counter()
+    print(f"market_data_request: {fix_api_client.market_data_request('EURUSD.x')}")
+    end_time = time.perf_counter()
+    execution_time_milliseconds = (end_time - start_time) * 1000
 
-  print(f"heartbeat: {fix_api_client.heartbeat()}")
+    whole_milliseconds = int(execution_time_milliseconds)
 
-  time.sleep(.5)
+    print(f"{whole_milliseconds:02}ms")
 
-  print(f"test_request: {fix_api_client.test_request()}")
+    time.sleep(.2)
 
-  time.sleep(.5)
+    # print(f"market_data_request: {fix_api_client.market_data_request('WRONG_SYMBOL')}")
+    # time.sleep(.2)
 
-  print(f"resend_request: {fix_api_client.resend_request()}")
-
-  # time.sleep(.5)
-
-  # print(f"reject: {fix_api_client.reject()}")
-
-  time.sleep(.5)
-
-  print(f"sequence_reset: {fix_api_client.sequence_reset()}")
-  
-  time.sleep(.5)
-
-  print(f"logout: {fix_api_client.logout()}")
+    # Duplicated id
+    # request_id = str(uuid4())
+    #
+    # print(f"market_data_request: {fix_api_client.market_data_request(request_id=request_id, symbol='EURUSD.x')}")
+    # time.sleep(.2)
+    #
+    # print(f"market_data_request: {fix_api_client.market_data_request(request_id=request_id, symbol='EURUSD.x')}")
+    # time.sleep(.2)
